532bc46a09eeb6570f552379ff2bf94c29f28829
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-06-25 04:25:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime(n)
+     if n ≤ 3
+        return n > 1
+     elsif n % 2 == 0 || n % 3 == 0
+        return false
+     let i ← 5
+     while i * i ≤ n
+        if n % i == 0 || n % (i + 2) == 0
+            return false
+        i ← i + 6
+     return true
+   end
+ end
